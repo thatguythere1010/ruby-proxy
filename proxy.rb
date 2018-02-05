2443a9720a904bb90016c1521a6ad1721c05b6ae
Sam Jones <sammyjster192@gmail.com>
2018-02-05 23:50:01 +0000

    Better error handling, and regex overhaul.

--- proxy.rb
+++ proxy.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'rest-client'
 require 'json'
+require 'date'
 
 class Proxy < Sinatra::Base
 	get '/favicon.ico' do
@@ -36,49 +37,55 @@ class Proxy < Sinatra::Base
 	def onRequest(requestUrl, params = nil)
 		if requestUrl != ""
 			begin
+				urlToRequest = "http://#{requestUrl}"
+				urlCaptures = /(http[s]?:\/\/)([^\/]+)([^$]+)/.match(request.url).captures
+				currentUrl = urlCaptures[0] + urlCaptures[1]
+
 				if params == nil
-					response = RestClient.get "http://#{requestUrl}"
+					response = RestClient.get urlToRequest
 				else
-		    		response = RestClient.post "http://#{requestUrl}", params
+		    		response = RestClient.post urlToRequest, params
 		    	end
 			    if response.code == 200
 			    	bodyContent = response.body
-			    	currentUrl = /(^http[s]?:\/\/[^\/]+)/.match(request.url).captures[0]
 			    	rootRequestUrl = /([^\/]+)/.match(requestUrl).captures[0]
 
-					bodyContent.gsub! /(?:http[s]?:\/\/)([^"]+)/, "#{currentUrl}/" + '\1'
+					bodyContent.gsub! /(?<!href=|src=)["']\K(http[s]?:\/\/|\/\/)(?!#{Regexp.quote(currentUrl.split("//")[1])})([^"']+["'])/, "#{currentUrl}/" + '\2' # General purpose replacement (excluding href + src)
 
-				    bodyContent.gsub! /(?<=href=")(\/[^\/][^"\s]+)/, "#{currentUrl}/#{rootRequestUrl}" + '\1'
-				    bodyContent.gsub! /(?<=src=")(\/[^\/][^"\s]+)/, "#{currentUrl}/#{rootRequestUrl}" + '\1'
-				    bodyContent.gsub! /(?<=href=")(\/{2}[^"\s]+)/, "#{currentUrl}/" + '\1'.tr('/', '')
-				    bodyContent.gsub! /(?<=src=")(\/{2}[^"\s]+)/, "#{currentUrl}/" + '\1'.tr('/', '')
-					
-				    bodyContent.gsub! /(?<=href=')(\/[^\/][^'\s]+)/, "#{currentUrl}/#{rootRequestUrl}" + '\1'
-				    bodyContent.gsub! /(?<=src=')(\/[^\/][^'\s]+)/, "#{currentUrl}/#{rootRequestUrl}" + '\1'
-				    bodyContent.gsub! /(?<=href=')(\/{2}[^'\s]+)/, "#{currentUrl}/" + '\1'.tr('/', '')
-				    bodyContent.gsub! /(?<=src=')(\/{2}[^'\s]+)/, "#{currentUrl}/" + '\1'.tr('/', '')
+				    bodyContent.gsub! /((?:(?:href)|(?:src))=['"])((?:http[s]?:\/\/)|(?:\/{2}))/, '\1' + "#{currentUrl}/" # 2 slashes or http[s]:// after href="
+				    bodyContent.gsub! /((?:(?:href)|(?:src))=['"][\/])(?!(?:http[s]?:\/\/)|(?:\/+))/, '\1' + "#{rootRequestUrl}/" # 1 slash after href="
+				    bodyContent.gsub! /((?:(?:href)|(?:src))=['"])(?!(?:http[s]?:\/\/)|(?:\/+))/, '\1' + "#{currentUrl}/#{rootRequestUrl}/" # No slash after href="
 
 			    	bodyContent.sub! "<head>", "<head>\n<link rel=\"shortcut icon\" href=\"#{currentUrl}/#{rootRequestUrl}/favicon.ico\" />"
 
 			    	content_type response.headers[:content_type]
 			    	body bodyContent
-			    else
-			    	status response.code
-			    	body 'The requested page returned this response code, or the requested page could not be found.'
 			    end
 			rescue SocketError => e
-				status 400
+				status 404
 				body "<h1>Invalid URL</h1>"
-		    rescue => e
-		    	status 500
-		    	body "#{e.message}<br><br>#{e.backtrace.inspect}"
-		    	#open("error.log", "w") do |f|
-		    	#	f.puts "#{e.message}\n\n#{e.backtrace.inspect}"
+			rescue RestClient::ServerBrokeConnection => e
+				status e.http_code
+		    	body "<h1>ServerBrokeConnection</h1><br><br>#{e.message}<br><br>#{e.backtrace.inspect}"
+		    rescue RestClient::RequestFailed => e
+		    	status e.http_code
+		    	if [301, 302, 307].include? e.http_code
+		    		redirect "#{currentUrl}/#{e.http_headers[:location].gsub!(/(http[s]?:\/\/)/, "")}"
+		    	else
+		    		body "<h1>RequestFailed</h1><br><br>#{e.message}<br><br>#{e.backtrace.inspect}"
+		    	end
+		    	#open("error.log", "a") do |f|
+		    	#	f.puts "[#{Time.now.strftime("%d/%m/%Y %H:%M:%S")}] #{e.message}\n#{e.backtrace.inspect}\n\n"
 		    	#end
+		    rescue Errno::ECONNRESET => e
+		    	status 500 
+		    	body "<h1>Errno::ECONNRESET</h1><br><br>#{e.message}<br><br>#{e.backtrace.inspect}"
+		    rescue Errno::ECONNABORTED => e
+		    	status 500 
+		    	body "<h1>Errno::ECONNABORTED</h1><br><br>#{e.message}<br><br>#{e.backtrace.inspect}"  	
 		    end
 		else
 			status 200
-			currentUrl = /(^http[s]?:\/\/[^\/]+)/.match(request.url).captures[0]
 			body "<h1>Please use the URL syntax: #{currentUrl}/[desired_web_page]</h1>"
 		end
 	end
